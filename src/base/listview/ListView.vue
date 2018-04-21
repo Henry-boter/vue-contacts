f6eb4404e3bcef32ce06e10e1719739a1285deb2
Henry-boter <1971785019@qq.com>
2018-04-21 23:58:43 +0800

    样式编写

--- src/base/listview/ListView.vue
+++ src/base/listview/ListView.vue
@@ -1,12 +1,134 @@
 <template>
-    <div class="listview">
-        <h1>{{ msg }}</h1>
-    </div>
+ <scroll class="listview">
+   <ul>
+     <li  class="list-group">
+       <h2 class="list-group-title">热门</h2>
+       <ul>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+         <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+         <span class="name">薛之谦</span>
+       </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+         <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+         <span class="name">薛之谦</span>
+       </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+           <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+           <span class="name">薛之谦</span>
+         </li>
+         <li class="list-group-item">
+         <img class="avatar" src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+         <span class="name">薛之谦</span>
+       </li>
+       </ul>
+     </li>
+   </ul>
+   <div class="list-shortcut">
+     <ul>
+       <li class="item current">热</li>
+       <li class="item">A</li>
+       <li class="item">B</li>
+       <li class="item">C</li>
+       <li class="item">D</li>
+     </ul>
+   </div>
+   <div class="list-fixed" ref="fixed">
+     <div class="fixed-title">A</div>
+   </div>
+ </scroll>
 </template>
 
 <script>
+import Scroll from '@/base/scroll/scroll'
 export default {
   name: 'hello',
+  components: {
+    Scroll
+  },
   data () {
     return {
       msg: 'ListView'
@@ -14,6 +136,64 @@ export default {
   }
 }
 </script>
-<style scoped>
-
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/variable.styl"
+  .listview
+    position: relative
+    width: 100%
+    height: 100%
+    overflow: hidden
+    background: $color-background
+    .list-group
+      padding-bottom 30px
+      .list-group-title
+        height 30px
+        height 30px
+        line-height 30px
+        padding-left 20px
+        font-size $font-size-small
+        color $color-text-l
+        background $color-highlight-background
+      .list-group-item
+        display flex
+        align-items center
+        padding 20px 0 0 30px
+        .avatar
+          width 50px
+          height 50px
+          border-radius 50%
+        .name
+          margin-left 20px
+          color $color-text-l
+    .list-shortcut
+      position absolute
+      z-index 30
+      right 0
+      top: 50%
+      transform translateY(-50%)
+      width 20px
+      padding 20px 0
+      border-radius 10px
+      text-align center
+      background $color-background-d
+      font-family Helvetica
+      .item
+        padding 3px
+        line-height 1
+        color $color-text-l
+        font-size $font-size-small
+        &.current
+          color $color-theme
+    .list-fixed
+      position absolute
+      top:0
+      left 0
+      width 100%
+      .fixed-title
+        height 30px
+        line-height 30px
+        padding-left 20px
+        font-size $font-size-small
+        color $color-text-l
+        background $color-highlight-background
 </style>
--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -6,6 +6,10 @@
 
 <script>
 import ListView from '@/base/listview/listview'
+import {singer} from '../data/singer'
+
+const HOT_SINGER_LEN = 10
+const HOT_NAME = '热门'
 
 export default {
   name: 'Contacts',
@@ -14,7 +18,42 @@ export default {
   },
   data () {
     return {
-      msg: 'contacts'
+      singer: []
+    }
+  },
+  created () {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList () {
+      this.singers = singer.data.list
+      console.log(this.singers)
+    },
+    _normalizesinger (list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name,
+            avatar: `https://y.gtimg.cn/music/photo_new/T001R150x150M000${item.Fsinger_mid}.jpg?max_age=2592000`
+          })
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push({
+        })
+      })
     }
   }
 }
