18bab93c01053a63a270ee39962c55f9d7704e0c
Henry-boter <1971785019@qq.com>
2018-04-22 20:29:04 +0800

    滚动实现

--- config/index.js
+++ config/index.js
@@ -9,11 +9,11 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '192.168.3.12', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 8050, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
