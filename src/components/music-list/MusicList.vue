be48eea803d0a52a4e45d466f82a3aad6e7da2b3
Henry-boter <1971785019@qq.com>
2018-04-30 11:27:50 +0800

    列表完善

--- src/components/music-list/MusicList.vue
+++ src/components/music-list/MusicList.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
-    <h1 class="title">写之前</h1>
-    <div class="bg-image" ref="bgImage">
+    <h1 class="title" v-html="title"></h1>
+    <div class="bg-image" :style="bgStyle" ref="bgImage">
       <div class="play-wrapper">
-        <div class="play" ref="playBtn">
+        <div ref="playBtn" v-show="songs.length>0" class="play" @click="random">
           <i class="icon-play"></i>
           <span class="text">随机播放全部</span>
         </div>
@@ -14,9 +14,13 @@
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
-    <scroll class="list" ref="list">
+    <scroll :data="songs" @scroll="scroll"
+            :listen-scroll="listenScroll" :probe-type="probeType" class="list" ref="list">
       <div class="song-list-wrapper">
-
+        <song-list :songs="songs" :rank="rank" @select="selectItem"></song-list>
+      </div>
+      <div v-show="!songs.length" class="loading-container">
+        <loading></loading>
       </div>
     </scroll>
   </div>
@@ -24,15 +28,116 @@
 
 <script>
 import Scroll from '@/base/scroll/Scroll'
+import SongList from '@/base/song-list/SongList'
+import Loading from '@/base/loading/loading'
+import {prefixStyle} from '@/common/js/dom'
+// import {playlistMixin} from '@/common/js/mixin'
+import {mapActions} from 'vuex'
+
+const RESERVED_HEIGHT = 40
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
+
 export default {
-  name: 'hello',
+//  mixins: [playlistMixin],
+  props: {
+    bgImage: {
+      type: String,
+      default: ''
+    },
+    songs: {
+      type: Array
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    rank: {
+      type: Boolean,
+      default: false
+    }
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      scrollY: 0
+    }
+  },
+  created () {
+    this.probeType = 3
+    this.listenScroll = true
+  },
+  mounted () {
+    this.imageHeight = this.$refs.bgImage.clientHeight
+    this.minTransalteY = -this.imageHeight + RESERVED_HEIGHT
+    this.$refs.list.$el.style.top = `${this.imageHeight}px`
+  },
+  computed: {
+    bgStyle () {
+      return `background-image:url(${this.bgImage})`
+    }
+  },
+  methods: {
+    handlePlaylist (playlist) {
+      const bottom = playlist.length > 0 ? '60px' : ''
+      this.$refs.list.$el.style.bottom = bottom
+      this.$refs.list.refresh()
+    },
+    scroll (pos) {
+      this.scrollY = pos.y
+    },
+    back () {
+      this.$router.back()
+    },
+    selectItem (item, index) {
+      this.selectPlay({
+        list: this.songs,
+        index
+      })
+    },
+    random () {
+      this.randomPlay({
+        list: this.songs
+      })
+    },
+    ...mapActions([
+      'selectPlay',
+      'randomPlay'
+    ])
+  },
+  watch: {
+    scrollY (newVal) {
+      let translateY = Math.max(this.minTransalteY, newVal)
+      let scale = 1
+      let zIndex = 0
+      let blur = 0
+      const percent = Math.abs(newVal / this.imageHeight)
+      if (newVal > 0) {
+        scale = 1 + percent
+        zIndex = 10
+      } else {
+        blur = Math.min(20, percent * 20)
+      }
+
+      this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
+      this.$refs.filter.style[backdrop] = `blur(${blur}px)`
+      if (newVal < this.minTransalteY) {
+        zIndex = 10
+        this.$refs.bgImage.style.paddingTop = 0
+        this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+        this.$refs.playBtn.style.display = 'none'
+      } else {
+        this.$refs.bgImage.style.paddingTop = '70%'
+        this.$refs.bgImage.style.height = 0
+        this.$refs.playBtn.style.display = ''
+      }
+      this.$refs.bgImage.style[transform] = `scale(${scale})`
+      this.$refs.bgImage.style.zIndex = zIndex
     }
   },
   components: {
-    Scroll
+    Scroll,
+    SongList,
+    Loading
   }
 }
 </script>
--- src/pages/contacts/ContactsDetail.vue
+++ src/pages/contacts/ContactsDetail.vue
@@ -1,9 +1,6 @@
 <template>
   <transition name="slide">
-    <!--<div class="contacts-detail">-->
-      <!--{{singer}}-->
-    <!--</div>-->
-    <music-list></music-list>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 <script>
@@ -18,7 +15,7 @@ export default {
   },
   data () {
     return {
-      song: []
+      songs: []
     }
   },
   filters: {},
@@ -29,6 +26,12 @@ export default {
 
   },
   computed: {
+    title () {
+      return this.singer.name
+    },
+    bgImage () {
+      return this.singer.avatar
+    },
     ...mapGetters([
       'singer'
     ])
@@ -63,14 +66,7 @@ export default {
 
 <style scoped="scoped" lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/variable.styl"
-  .contacts-detail
-    position fixed
-    z-index 100
-    top: 0
-    left 0
-    right 0
-    bottom 0
-    background $color-background
+
   .slide-enter-active, .slide-leave-active
     transition all 0.3s
   .slide-enter, .slide-leave-to
